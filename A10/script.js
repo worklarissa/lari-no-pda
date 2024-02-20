4531d5b433db53f363cdc7621839c06dabafa17d
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-02-20 18:50:32 -0300

    aula 10-terminar

---
+++ A10/script.js
@@ -0,0 +1,14 @@
+numeroSecreto  = 56
+chute = parseInt( window.prompt("Digite qual número você acha que é"))
+acertou = false
+
+
+if (chute == numeroSecreto){
+    console.log ("Acertou");
+}
+if (numeroSecreto > chute) {
+    console.log("O número secreto é maior! Tente novamente.")
+}
+if (numeroSecreto < chute) {
+    console.log("O número é menor! Tente novamente")
+}
