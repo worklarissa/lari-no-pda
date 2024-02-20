a6d64c29fbff0a73429b0d893421d44b9f56db46
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-02-20 18:32:56 -0300

    aula 8

---
+++ A8/script.js
@@ -0,0 +1,18 @@
+let numero1 = 3;
+let numero2 = 7
+
+//soma
+console.log(numero1 + numero2)
+
+//subtração
+console.log(numero2-numero1)
+
+//multiplicação
+console.log(numero1 * numero2)
+
+//divisão
+console.log(numero2/numero1)
+
+let texto1 = "PDA"
+let texto2 = " É a sigla para Programadores do Amanhã"
+console.log(texto1+texto2)
