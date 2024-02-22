8319c79ed6809f73602b008a639067fe846d8e0c
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-02-22 20:16:25 -0300

    aula 12 modulo 1

---
+++ A12/script.js
@@ -0,0 +1,13 @@
+let aluno1 = alert("Oi!!")
+let alunoNovo = prompt ( "Responda ao aluno:");
+
+if (alunoNovo == "Oi" || alunoNovo == "Bom dia"){
+     var resposta = prompt ("Como está?")
+}
+
+if(resposta == "Estou bem"){
+    alert("Que bom!!")
+}else{
+    alert("Que triste, tenho certeza que ficará tudo bem")
+}
+
