f77af19aa78f99bfe310e8eec139e9b0d186b9dd
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-02-26 21:46:36 -0300

    aula18 e parte da 19

---
+++ A19/script.js
@@ -0,0 +1,15 @@
+var animais = ["Elefante", "Formiga", "Gato", "Cachorro"]
+
+animais.push("Leão")
+console.log(animais)
+
+console.log(animais.pop())
+
+animais.unshift("Macaco")
+console.log(animais)
+
+animais.shift()
+console.log(animais)
+
+animais.join(",", String)
+console.log(animais)
