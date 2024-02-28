42a433a1257a6047788c9582efd535fdcabc2a01
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-02-28 16:00:41 -0300

    salvo o script da aula 1

---
+++ A1/script.js
@@ -0,0 +1,6 @@
+let nota1 = parseFloat(prompt("Insira a nota da primeira prova"))
+let nota2 = parseFloat(prompt("Insira a nota da segunda prova"))
+let nota3 = parseFloat(prompt("Insira a nota da recuperação"))
+
+if(nota)
+
