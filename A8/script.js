c19576c00a04f8ffb3db33e22cd61ef2ae6160a3
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-03-09 19:34:27 -0300

    ajeitei a atividade assíncrona da aula 8

--- A8/script.js
+++ A8/script.js
@@ -4,11 +4,17 @@ const botaoAdicionar = document.querySelector('#valida-nome')
 //função para que ao apertar o botão chame a api
 function chamarApi(nme){
     //Armazenar nome digitado no input
-    var nme = document.getElementById("name").value
+    var nme = document.getElementById("name").value;
 
+
+    //Aplicar o método XMLHttpRequest na variavel, para buscar a api
     let xhr = new XMLHttpRequest();
     xhr.onload = function() {
         if (xhr.status === 200) {
+            var reponse = JSON.parse(xhr.responseText); //converte a resposta da api que esté em json para um objeto 
+            //para facilitar a manipulação de dados
+            document.getElementById("resultado").innerHTML = `Nome: ${response.name},
+            Idade: ${response.age} anos` ;
             console.log(xhr);
         }
     };
@@ -16,13 +22,9 @@ function chamarApi(nme){
     xhr.open("GET", `https://api.agify.io?name=${nme}`);
     
     xhr.send();
+
+
 }
 
-
-//Aplicar o método XMLHttpRequest na variavel, para buscar a api
-//Intanciar método
-//Definir uma função que será chamada quando a resposta chegar
-//abrir uma conexão com a API
-//esperar pela resposta
-
-botaoAdicionar.addEventListener("click", chamarApi)
+//Adicionar evento de clique ao botão
+document.querySelector('#valida-nome').addEventListener("click", chamarApi);
