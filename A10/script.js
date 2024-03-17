59e05ff7705d759ae62a9ff536991b31fb9b8324
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-03-17 11:58:42 -0300

    terminei o exercicio, precisa testar

--- A10/script.js
+++ A10/script.js
@@ -1,14 +1,21 @@
-const email = $("#email").val();
-console.log(email)
+document.getElementById('executeAjax').addEventListener('click', function() {
+    const yourEmail = $("#email").val();
+    console.log(yourEmail);
 
-$.ajax({
-    url:"scraper.run/email?addr=${email}"
-    type:"GET"
-    datatype:"json"
-    sucess: function (data){
-        console.log(data.email)
-    },
-    error: function (error){
-        console.log(error)
-    },
+    $.ajax({
+        url:`scraper.run/email?addr=${yourEmail}`,
+        type:"GET",
+        dataType:"json",
+        beforeSend: function(){
+            $("#resultados").html("Carregando...");
+        },
+        success: function (data){
+            $("#resultados").html("<span>O email é real</span>")
+            console.log(data.email)
+        },
+        error: function (xhr, status, error){
+            $("#resultados").html("<span>O email é falso</span>")
+            console.log(xhr.responseText)
+        }
+    })
 })
