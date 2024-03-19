4d569a1a3151abba8818e854a98ecb0082b443f6
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-03-19 14:42:46 -0300

    primeiro projeto m5

---
+++ A4/zap-zap/src/components/Button.jsx
@@ -0,0 +1,5 @@
+export default function Button({title}){
+    return (
+        <button style={{color}}></button>
+    )
+}
---
+++ A4/zap-zap/src/screens/Home/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home({name}){
+    return(
+        <>
+            <h1>Bem Vindo {name}</h1>
+            <h2></h2>
+        </>
+    )
+}
